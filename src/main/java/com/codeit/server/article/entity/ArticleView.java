63d1fb192511c8411f11e88bd34c398869d05feb
taesung <taesung819@gmail.com>
2026-06-26 18:50:20 +0900

    feat: Add ArticleInterest and ArticleView entities

---
+++ src/main/java/com/codeit/server/article/entity/ArticleView.java
@@ -0,0 +1,31 @@
+package com.codeit.server.article.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.Instant;
+import java.util.UUID;
+
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@ToString
+@Entity
+@Table(name = "article_views")
+@EntityListeners(AuditingEntityListener.class)
+public class ArticleView {
+    @Id
+    @Column(updatable = false, nullable = false)
+    private UUID id;
+
+    @Column(nullable = false)
+    private UUID ArticleId;
+
+    @Column(nullable = false)
+    private UUID UserId;
+
+    @Column(nullable = false)
+    private Instant createdAt;
+}
