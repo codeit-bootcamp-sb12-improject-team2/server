5ea20d93eab0e534c8932717e483ee3cb7a7efba
NaSangJoon <107744931+Nasangjoon@users.noreply.github.com>
2026-06-26 17:30:57 +0900

    feat : Add notification entity

--- src/main/java/com/codeit/server/notification/controller/NotificationController.java
+++ src/main/java/com/codeit/server/notification/controller/NotificationController.java
@@ -1,5 +1,13 @@
 package com.codeit.server.notification.controller;
 
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/notifications")
+@RequiredArgsConstructor
 public class NotificationController {
 
 }
