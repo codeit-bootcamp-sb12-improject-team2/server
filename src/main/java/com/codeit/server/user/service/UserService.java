709e4bc610fac3392c6ec07b88ea2be00c3659dd
zziyo8 <seok8@outlook.com>
2026-06-29 17:47:20 +0900

    feat: Add user service

---
+++ src/main/java/com/codeit/server/user/service/UserService.java
@@ -0,0 +1,22 @@
+package com.codeit.server.user.service;
+
+import com.codeit.server.user.dto.UserDto;
+import com.codeit.server.user.dto.UserLoginRequest;
+import com.codeit.server.user.dto.UserRegisterRequest;
+import com.codeit.server.user.dto.UserUpdateRequest;
+import java.util.UUID;
+
+public interface UserService {
+
+  UserDto register(UserRegisterRequest request);
+
+  UserDto login(UserLoginRequest request);
+
+  UserDto update(UUID userId, UUID requestUserId, UserUpdateRequest request);
+
+  void delete(UUID userId, UUID requestUserId);
+
+  void hardDelete(UUID userId, UUID requestUserId);
+
+  void purgeExpiredSoftDeletedUsers();
+}
