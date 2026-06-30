0cc4fe508768007ef6b9d282bc0e6cee02534cc8
NaSangJoon <107744931+Nasangjoon@users.noreply.github.com>
2026-06-30 17:13:22 +0900

    feat : Add Batch package

---
+++ src/main/java/com/codeit/server/batch/job/notification/DeleteOldNotificationsJobConfig.java
@@ -0,0 +1,35 @@
+package com.codeit.server.batch.job.notification;
+
+import com.codeit.server.batch.job.notification.tasklet.DeleteOldNotificationsTasklet;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.job.builder.JobBuilder;
+import org.springframework.batch.core.repository.JobRepository;
+import org.springframework.batch.core.step.builder.StepBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.PlatformTransactionManager;
+
+@Slf4j
+@Configuration
+@RequiredArgsConstructor
+public class DeleteOldNotificationsJobConfig {
+
+    private final DeleteOldNotificationsTasklet deleteOldNotificationsTasklet;
+
+    @Bean
+    public Job deleteOldNotificationsJob(JobRepository jobRepository, Step deleteOldNotificationsStep) {
+        return new JobBuilder("deleteOldNotificationsJob", jobRepository)
+                .start(deleteOldNotificationsStep)
+                .build();
+    }
+
+    @Bean
+    public Step deleteOldNotificationsStep(JobRepository jobRepository, PlatformTransactionManager transactionManager) {
+        return new StepBuilder("deleteOldNotificationsStep", jobRepository)
+                .tasklet(deleteOldNotificationsTasklet, transactionManager)
+                .build();
+    }
+}
