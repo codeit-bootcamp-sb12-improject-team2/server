cce4021d4e07bace5076b0f0a6af6d2ef3e8ac0b
taesung <taesung819@gmail.com>
2026-06-30 18:02:58 +0900

    refactor: refactor Article entity code structure

--- src/main/java/com/codeit/server/article/entity/Article.java
+++ src/main/java/com/codeit/server/article/entity/Article.java
@@ -1,16 +1,11 @@
 package com.codeit.server.article.entity;
 
 import com.codeit.server.global.common.BaseUpdatableEntity;
-import com.github.f4b6a3.uuid.UuidCreator;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.PositiveOrZero;
 import lombok.*;
-import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.LastModifiedDate;
-import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.Instant;
-import java.util.UUID;
 
 @Getter
 @Builder
@@ -19,7 +14,6 @@ import java.util.UUID;
 @ToString
 @Entity
 @Table(name = "articles")
-@EntityListeners(AuditingEntityListener.class)
 public class Article extends BaseUpdatableEntity {
 
     @Column(nullable = false, length = 30)
@@ -47,11 +41,9 @@ public class Article extends BaseUpdatableEntity {
     @Column(nullable = false)
     private int commentCount = 0;
 
-
     @Builder.Default
     @Column(nullable = false)
     private boolean isDeleted = false;
-
 
     public void increaseViewCount() {
         this.viewCount++;
@@ -62,31 +54,12 @@ public class Article extends BaseUpdatableEntity {
     }
 
     public void decreaseCommentCount() {
-        this.commentCount--;
+        if (this.commentCount > 0) {
+            this.commentCount--;
+        }
     }
 
     public void delete() {
         this.isDeleted = true;
     }
-
 }
-
-//CREATE TABLE articles (
-//        id UUID PRIMARY KEY,
-//        source VARCHAR(30) NOT NULL,
-//        source_url VARCHAR(255) NOT NULL UNIQUE,
-//        title VARCHAR(255) NOT NULL,
-//        publish_date TIMESTAMP WITH TIME ZONE NOT NULL,
-//        summary TEXT NOT NULL,
-//        view_count INTEGER NOT NULL DEFAULT 0,
-//        comment_count INTEGER NOT NULL DEFAULT 0,
-//        created_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now(),
-//        updated_at TIMESTAMP WITH TIME ZONE,
-//        is_deleted BOOLEAN NOT NULL DEFAULT FALSE,
-//
-//        CONSTRAINT chk_articles_view_count
-//            CHECK (view_count >= 0),
-//
-//        CONSTRAINT chk_articles_comment_count
-//            CHECK (comment_count >= 0)
-//);
