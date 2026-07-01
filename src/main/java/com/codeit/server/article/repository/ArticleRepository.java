de25c2f01b418051991633da082d10e8d8b450a8
taesung <taesung819@gmail.com>
2026-07-01 17:58:17 +0900

    feat: Add validation and check methods to ArticleRepository

--- src/main/java/com/codeit/server/article/repository/ArticleRepository.java
+++ src/main/java/com/codeit/server/article/repository/ArticleRepository.java
@@ -4,10 +4,12 @@ import com.codeit.server.article.entity.Article;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface ArticleRepository
         extends JpaRepository<Article, UUID>, ArticleRepositoryCustom {
     boolean existsBySourceUrl(String sourceUrl);
+    Optional<Article> findBySourceUrl(String sourceUrl);
 }
