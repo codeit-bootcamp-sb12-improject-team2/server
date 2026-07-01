58a2bc058e05702450454c878788af208d685af5
taesung <taesung819@gmail.com>
2026-07-01 17:55:28 +0900

    refactor: Remove inheritance from InterestKeyword

--- src/main/java/com/codeit/server/interest/entity/InterestKeyword.java
+++ src/main/java/com/codeit/server/interest/entity/InterestKeyword.java
@@ -2,16 +2,23 @@ package com.codeit.server.interest.entity;
 
 
 import com.codeit.server.global.common.BaseEntity;
+import com.github.f4b6a3.uuid.UuidCreator;
 import jakarta.persistence.*;
 import lombok.*;
 import lombok.experimental.SuperBuilder;
+
+import java.util.UUID;
 
 @Entity
 @Table(name = "interest_keyword")
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @SuperBuilder
-public class InterestKeyword extends BaseEntity {
+public class InterestKeyword {
+
+    @Id
+    @Column(name = "id", columnDefinition = "uuid", updatable = false, nullable = false)
+    private UUID id;
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "interest_id", nullable = false)
@@ -19,6 +26,13 @@ public class InterestKeyword extends BaseEntity {
 
     @Column(name = "keyword", length = 30, nullable = false)
     private String keyword;
+
+    @PrePersist
+    void init() {
+        if (this.id == null) {
+            this.id = UuidCreator.getTimeOrderedEpoch();
+        }
+    }
 
     void assignInterest(Interest interest) {
         this.interest = interest;
