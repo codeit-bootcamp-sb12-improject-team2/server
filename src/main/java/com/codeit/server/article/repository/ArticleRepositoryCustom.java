40e7e0130c696bc5a02e6f25b23866ff7c681d38
hyeon <rygus0128@gmail.com>
2026-07-02 18:16:08 +0900

    feat: add article ranking queries
    # Conflicts:
    #	src/main/java/com/codeit/server/article/repository/ArticleRepositoryImpl.java

--- src/main/java/com/codeit/server/article/repository/ArticleRepositoryCustom.java
+++ src/main/java/com/codeit/server/article/repository/ArticleRepositoryCustom.java
@@ -1,6 +1,7 @@
 package com.codeit.server.article.repository;
 
 import com.codeit.server.article.dto.ArticleDto;
+import com.codeit.server.article.dto.ArticleRankingDto;
 import com.codeit.server.article.dto.ArticleSearchRequest;
 import com.codeit.server.article.dto.CursorPageResponseArticle;
 import com.codeit.server.batch.job.articlebackup.dto.ArticleBackupDto;
@@ -13,6 +14,8 @@ import java.util.UUID;
 public interface ArticleRepositoryCustom {
     Optional<ArticleDto> findArticle(UUID articleId, UUID requestUserId);
     CursorPageResponseArticle searchArticles(String cursor, String after, int size, UUID requestUserId, ArticleSearchRequest request);
+    List<ArticleRankingDto> findTopArticlesByViewCount(Instant start, Instant end);
+    List<ArticleRankingDto> findTopArticlesByCommentCount(Instant start, Instant end);
     List<String> findSource();
     List<ArticleBackupDto> findBackup(Instant start, Instant end);
     void restoreAuditFields(UUID articleId, Instant createdAt, Instant updatedAt);
